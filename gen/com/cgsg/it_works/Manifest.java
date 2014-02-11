d68f380c8a44c1160ea4f028e283c902120635a1
unknown <SEMI14@R111-32.(none)>
2014-02-11 18:39:26 +0400

    Add new project (by SP4).

---
+++ gen/com/cgsg/it_works/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.cgsg.it_works;
+
+/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
+public final class Manifest {
+}
---
+++ src/com/cgsg/it_works/Thing.java
@@ -0,0 +1,11 @@
+package com.cgsg.it_works;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: SEMI14
+ * Date: 11.02.14
+ * Time: 18:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class Thing {
+}
