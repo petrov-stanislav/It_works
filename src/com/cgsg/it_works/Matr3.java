e1159ad808d4a8dfb4d5e3c7ff27105a3752c9b1
yaroslav <yardru@yandex.ru>
2014-03-06 16:58:19 +0300

    a

--- src/com/cgsg/it_works/Matr3.java
+++ src/com/cgsg/it_works/Matr3.java
@@ -44,6 +44,18 @@ public class Matr3 {
         this.a33 = a33;
     }
 
+    public Matr3(Matr3 m) {
+        this.a11 = m.a11;
+        this.a12 = m.a12;
+        this.a13 = m.a13;
+        this.a21 = m.a21;
+        this.a22 = m.a22;
+        this.a23 = m.a23;
+        this.a31 = m.a31;
+        this.a32 = m.a32;
+        this.a33 = m.a33;
+    }
+
     public Matr3 SetUnit() {
         this.a11 = 1;
         this.a12 = 0;
@@ -77,5 +89,61 @@ public class Matr3 {
         this.a33 = 1;
 
         return this;
+    }
+
+    public Matr3 SetTranslate(Vec2 v) {
+        this.a11 = 1;
+        this.a12 = 0;
+        this.a13 = 0;
+
+        this.a21 = 0;
+        this.a22 = 1;
+        this.a23 = 0;
+
+        this.a31 = v.x;
+        this.a32 = v.y;
+        this.a33 = 1;
+
+        return this;
+    }
+
+    public Matr3 Multiply(Matr3 m) {
+        Matr3 r = new Matr3();
+
+        r.a11 = a11 * m.a11 + a12 * m.a21;
+        r.a12 = a11 * m.a12 + a12 * m.a22;
+        r.a13 = 0;
+
+        r.a21 = a21 * m.a11 + a22 * m.a21;
+        r.a22 = a21 * m.a12 + a22 * m.a22;
+        r.a23 = 0;
+
+        r.a31 = a31 * m.a11 + a32 * m.a21 + m.a31;
+        r.a32 = a31 * m.a12 + a32 * m.a22 + m.a32;
+        r.a33 = 1;
+
+        return r;
+    }
+
+    public Matr3 Multiplying(Matr3 m) {
+        Matr3 r = new Matr3(this);
+
+        a11 = r.a11 * m.a11 + r.a12 * m.a21;
+        a12 = r.a11 * m.a12 + r.a12 * m.a22;
+        a13 = 0;
+
+        a21 = r.a21 * m.a11 + r.a22 * m.a21;
+        a22 = r.a21 * m.a12 + r.a22 * m.a22;
+        a23 = 0;
+
+        a31 = r.a31 * m.a11 + r.a32 * m.a21 + m.a31;
+        a32 = r.a31 * m.a12 + r.a32 * m.a22 + m.a32;
+        a33 = 1;
+
+        return this;
+    }
+
+    public double Determ() {
+        return a11 * a22 * a33 + a12 * a23 * a31 + a13 * a21 * a32 - a13 * a22 * a31 - a12 * a21 * a33 - a11 * a23 * a32;
     }
 }
